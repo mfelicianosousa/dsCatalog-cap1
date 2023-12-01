d6dab6d3faece462133cde7d10dcd5b83ca2162b
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2023-12-01 14:50:17 -0400

    Implements oauth2 authorization server

--- src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
+++ src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -2,12 +2,27 @@ package com.devsuperior.dscatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @SpringBootApplication
 public class DscatalogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-JWT-SECRET");
+		return tokenConverter;
+	}
+
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
 	}
 
 }
